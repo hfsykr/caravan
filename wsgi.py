7b120b51108da139ecf60e59ff91062aa459b36d
Hafizh <74042975+hfsykr@users.noreply.github.com>
2023-11-15 17:26:20 +0700

    fix generated url path not changing when deploying with reverse proxy

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,14 @@
+from dotenv import load_dotenv
+import os
 from app import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+load_dotenv()
 
 app = create_app()
+
+if os.getenv("REVERSE_PROXY", "Flase").lower() == "true":
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_host=1, x_prefix=1, x_for=1, x_proto=1)
 
 if __name__ == "__main__":
     app.run()
