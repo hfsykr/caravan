6b897670165f07d296c4b3de9ec593fa72019e96
Hafizh <74042975+hfsykr@users.noreply.github.com>
2024-01-05 22:53:56 +0700

    refactoring & add search feature

--- controllers/car_controller.py
+++ controllers/car_controller.py
@@ -1,6 +1,6 @@
 from flask import request, abort, redirect, url_for, render_template
 from services.ml_service import inference
-from services.car_service import get_car_pagination, get_car_by_slug, get_slug_by_label
+from services.car_service import get_car_list, search_car_list, get_car_by_slug, get_slug_by_label
 
 def predict():
     if request.method != "POST":
@@ -12,7 +12,11 @@ def predict():
 
 def list():
     page = request.args.get("page", 1, type=int)
-    data = get_car_pagination(page)
+    search = request.args.get("search", type=str)
+    if not search:
+        data = get_car_list(page)
+    else:
+        data = search_car_list(search, page)
     return render_template("car/list.html", data=data)
 
 def detail(slug):
--- services/car_service.py
+++ services/car_service.py
@@ -1,10 +1,15 @@
 from database import db
 from models.car_model import Car
+from sqlalchemy import func
 
-def get_car_list():
+def get_car_list(page, per_page=10):
     query = db.select(Car)
-    result = db.session.execute(query).scalars()
-    result = [r for r in result]
+    result = db.paginate(query, page=page, per_page=per_page)
+    return result
+
+def search_car_list(search, page, per_page=10):
+    query = db.select(Car).filter(func.concat(Car.manufacturer, " ", Car.model, " ", Car.type, " ", Car.year, " ",).ilike(f"%{search}%"))
+    result = db.paginate(query, page=page, per_page=per_page)
     return result
 
 def get_car_by_slug(slug):
@@ -15,9 +20,4 @@ def get_car_by_slug(slug):
 def get_slug_by_label(label):
     query = db.select(Car.slug).filter_by(label=label)
     result = db.one_or_404(query)
-    return result
-
-def get_car_pagination(page, per_page=10):
-    query = db.select(Car)
-    result = db.paginate(query, page=page, per_page=per_page)
     return result
